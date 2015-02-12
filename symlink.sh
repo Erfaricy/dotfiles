5efedcf71144995074c0adb68f19567aa8c5b646
Emmett Faricy <Emmett@mixpanel.com>
2015-02-11 16:58:53 -0800

    Symlink Script Set up for all dotfiles in folder

---
+++ symlink.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+find . -maxdepth 1 -name ".*" | while read -r file;
+do
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done

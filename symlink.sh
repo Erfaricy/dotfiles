7a3b125eed5f4863b6f03be4cd953a5d84b1626a
Emmett Faricy <Emmett@mixpanel.com>
2015-03-12 15:35:57 -0700

    Removed .Extra not in dotfiles

--- symlink.sh
+++ symlink.sh
@@ -4,7 +4,7 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 # move any existing dotfiles in homedir to dotfiles_old directory,then create symlinks
-for file in {.bash_prompt,.bash_profile,.gitconfig,.gitignore,.screenrc,.tmux.conf,.vim,.vimrc,.exports,.aliases,.functions,.extra}; do
+for file in {.bash_prompt,.bash_profile,.gitconfig,.gitignore,.screenrc,.tmux.conf,.vim,.vimrc,.exports,.aliases,.functions}; do
     echo "Creating symlink to $file in home directory."
     ln -s ~/dotfiles/$file ~/$file
 done
